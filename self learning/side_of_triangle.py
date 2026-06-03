0a602ceac0b77cfaa282262e3d3bce447a6c3959
kanishka jain <kani0211@gmail.com>
2026-06-04 00:27:25 +0530

    added sel learning program

---
+++ self learning/side_of_triangle.py
@@ -0,0 +1,32 @@
+side1 = float(input("Enter first side : "))
+
+# validate side1
+if(side1 <= 0):
+    exit("Side must be positive")
+
+# ---------------------------------------------
+
+side2 = float(input("Enter second side : "))
+
+# validate side2
+if(side2 <= 0):
+    exit("Side must be positive")
+
+# ---------------------------------------------
+
+side3 = float(input("Enter third side : "))
+
+# validate side3
+if(side3 <= 0):
+    exit("Side must be positive")
+
+# ---------------------------------------------
+
+# verifying triangle formation
+if(side1 + side2 > side3 and
+   side1 + side3 > side2 and
+   side2 + side3 > side1):
+    print("Above sides form a triangle")
+else:
+    print("Above sides do not form a triangle")
+
