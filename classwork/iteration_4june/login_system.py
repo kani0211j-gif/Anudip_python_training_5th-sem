ed83e25d299882e67fcf97e3e75d230688d0b327
kanishka jain <kani0211@gmail.com>
2026-06-04 14:29:53 +0530

    added primee no. program

---
+++ classwork/iteration_4june/login_system.py
@@ -0,0 +1,7 @@
+password = (input("Enter Password : "))
+while password != "admin123":
+    print("Incorrect Password. Try Again")
+    password = (input("Enter password: "))
+print("Login Successful")    
+
+
