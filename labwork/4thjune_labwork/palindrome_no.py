3b6ab5154c4af3ad4572e800ecc4bab2e5d4ebea
kanishka jain <kani0211@gmail.com>
2026-06-04 16:02:05 +0530

    adding palindrome program

---
+++ labwork/4thjune_labwork/palindrome_no.py
@@ -0,0 +1,17 @@
+num = int(input("Enter Number: "))
+
+temp = num
+rev = 0
+
+while temp > 0:
+    digit = temp % 10
+    rev = rev * 10 + digit
+    temp = temp // 10
+
+print("Reverse:", rev)
+
+if rev == num:
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")
+
